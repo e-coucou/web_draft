bba9091fe9bf817334420864aea896e7e6d452e2
e-Coucou <eric@e-coucou.com>
2023-08-27 08:59:49 +0200

    updt

--- public/dev/Petanque_PST/js/joueur.js
+++ public/dev/Petanque_PST/js/joueur.js
@@ -1,71 +1,253 @@
-const K = 3;
-const P = 1.5;
-const I = 100;
+const Ke = 1.5;
+const I = 40;
+const Kr = 10.;
 
 class Joueur {
     constructor(nom_, id_) {
         this.nom=nom_;
         this.id = id_;
         this.ELO = I;
+        this.rank = 0;
         this.match = 0;
         this.tireur = 0;
         this.pointeur = 0;
+        this.gagne=0;
+        this.perdu=0;
+        this.nul=0;
+        this.victoire = 0;
+        this.pour=0;
+        this.contre=0;
+        this.annees=[];
+        this.victoires=[];
+        this.hist=[];
+        this.palmares=[];
+        this.clast = [];
+        this.team = [];
+        this.matchs = [];
     }
 
-    update(delta_) {
-        this.ELO += delta_;
+    setVictoire(annee_) {
+        this.victoire += 1;
+        this.victoires.push(annee_);
+    }
+    setPointeur(annee_,j_,nom_) {
+        this.pointeur += 1;
+        this.annees.push(annee_);
+        this.team.push({annee: annee_, coeq: j_ , cat: 'Pointeur', team: nom_, team_cat:'Tireur'});
+    }
+    setTireur(annee_,j_, nom_) {
+        this.tireur += 1;
+        this.annees.push(annee_);
+        this.team.push({annee: annee_, coeq: j_ , cat: 'Tireur', team: nom_, team_cat:'Pointeur'});
+}
+    setClst(n_,a_,annee) {
+        let n = int(n_);
+        this.rank = n+1;
+        this.hist.push({c:(n+1), elo:this.ELO});
+        if (a_) {
+            let r = {};
+            r[annee] = {c: (n+1) , elo: this.ELO}; 
+            this.clast.push(r);
+        }
+    }
+    addMatch(p_, c_, a_, eqN_,ref_) {
+        this.pour += p_;
+        this.contre += c_;
+        this.matchs.push({id: a_, eq: eqN_, ref: ref_});
+    }
+    setPalmares(a_,p_,c_,id_) {
+        let t = this.team.filter( a => { return (a.annee == a_);});
+        // let m = matchs_[id_];
+        t[0]['matchId'] = id_;
+        t[0]['idEq'] = p_;
+    }
+    update(delta_,win_) {
+        this.ELO += delta_;9
+        this.match += 1;
+        if (win_>0) this.gagne += 1;
+        if (win_<0) this.perdu += 1;
+        if (win_==0) this.nul += 1;
+    }
+    getColor() {
+        switch (this.victoire) {
+            case 0 : fill(10,120,10); break;
+            case 1 : fill(10,200,10); break;
+            case 2 : fill(10,255,10); break;
+        }
+    }
+    show(idx, x_, y_, elo=this.ELO) {
+        let x= x_, y = y_;
+        noStroke();
+        this.getColor();
+        rect(x-7,y-9,6,18);
+        fill(10,70,10);
+        rect(x,y-9,200,18);
+        fill(255);
+        textAlign(LEFT,CENTER);
+        textSize(12);
+        text(nf(idx,2,0)+"/ "+this.nom+" ["+nf(elo,0,1)+"] "+this.gagne+"/"+this.nul+"/"+this.perdu+" ("+nf(100.*this.gagne/(this.gagne+this.perdu),0,0)+"% )", x, y);
+    }
+    draw(idx,n,w,h, elo=this.ELO) {
+        noStroke();
+        let x = 20 + (w / 40 * (elo-20));
+        let y = 25 + h/n*int(idx);
+        let r = 2 + this.match/4;
+        this.getColor();
+        circle(x,y,r);
+        fill(255);
+        textAlign(LEFT,CENTER);
+        textSize(10);
+        text(this.nom.substring(0,20),x+5,y);
+    }
+
+    fiche(x_,y_,w_,matchs_) {
+        let x= x_;
+        let y = y_+35;
+        noStroke();
+        this.getColor();
+        rect(x,y-13,5,26);
+        fill(10,50,10);
+        rect(x+5,y-13,w_+14,26);
+        fill(255);
+        textAlign(LEFT,CENTER);
+        textSize(16);
+        text(this.rank+'/ '+this.nom+' ('+nf(this.ELO,0,1)+')',x+7,y);
+        fill(10,50,10);
+        textSize(12);
+        let dy=22;
+        text('Participations :',x,y+dy);
+        for (let i in this.annees) {
+            text(this.annees[i],x+90+30*i,y+dy);
+        }
+        dy += 18;
+        text('Victoires :',x,y+dy);
+        for (let i in this.victoires) {
+            text(this.victoires[i],x+90+30*i,y+dy);
+        }
+        dy += 18;
+        text('. Matchs gagnés : '+this.gagne,x+5,y+dy);
+        dy += 16;
+        text('. Matchs perdus : '+this.perdu,x+5,y+dy);
+        dy += 16;
+        text('. Matchs nuls   : '+this.nul,x+5,y+dy);
+        dy += 18;
+        text('Points Marqués/Encaissés : '+this.pour+' / '+this.contre+' ('+((this.pour>this.contre)?'+':'')+(this.pour-this.contre)+')',x,y+dy);
+        dy += 18;
+        text('Catégorie Pointeur/Tireur : '+this.pointeur+' / '+this.tireur,x,y+dy);
+        dy += 18;
+        text('Palmares :',x,y+dy);
+        for (let i in this.annees) {
+            dy += 16;
+            let t = this.team[i];
+            let m = matchs_[t.matchId];
+            let e1 = t.idEq, e2= (t.idEq+1) %2;
+            let vs = m.equipes[e2].eq;
+            let res = (m.equipes[e1].sc > m.equipes[e2].sc)?'Gagnée':'Perdue';
+            let sc = '  ('+m.equipes[e1].sc+'-'+m.equipes[e2].sc+')';
+            let vs_t = ' vs ('+vs.nom+') '+ vs.tireur.nom+'/'+vs.pointeur.nom;
+            text(t.annee+' ('+t.team+') -'+t.cat+' avec '+t.coeq.nom+' "'+m.type+'" '+res+sc+vs_t,x+5,y+dy);
+        }
+        dy += 18;
+        text('Matchs :',x,y+dy);
+        let sc_t='';
+        for (let a of this.annees) {
+            dy += 16;
+            let t = this.matchs.filter( r => {return (r.id==a);});
+            // console.log(t);
+            sc_t = a;
+            for (let i in t) {
+                let m = t[i].ref;
+                let e1 = t[i].eq, e2= (t[i].eq+1) %2;
+                // let vs = m.equipes[e2].eq;
+                // let res = (m.equipes[e1].sc > m.equipes[e2].sc)?'Gagnée':'Perdue';
+                // console.log(t,m,e1,e2);
+                sc_t = sc_t + '  ('+m.equipes[e1].sc+'-'+m.equipes[e2].sc+')';
+                // let vs_t = ' vs ('+vs.nom+') '+ vs.tireur.nom+'/'+vs.pointeur.nom;
+            }
+            text(sc_t,x+5,y+dy);
+        }
     }
 }
 
 class Equipe {
-    constructor(nom_,j1, j2) {
+    constructor(nom_,j1, j2, annee_) {
         this.tireur = j1;
         this.pointeur = j2;
         this.nom = nom_;
         this.ELOm = (j1.ELO + j2.ELO ) /2;
+        this.tireur.setTireur(annee_,j2,this.nom);
+        this.pointeur.setPointeur(annee_,j1,this.nom);
+        this.annee = annee_;
     }
 
-    update(delta_) {
-        this.tireur.update(delta_);
-        this.pointeur.update(delta_);
+    setVictoire(annee_) {
+        this.pointeur.setVictoire(annee_);
+        this.tireur.setVictoire(annee_);
+    }
+
+    update(delta_,win_) {
+        this.tireur.update(delta_,win_);
+        this.pointeur.update(delta_,win_);
+        this.ELOm = (this.tireur.ELO + this.pointeur.ELO ) /2;
+    }
+
+    addMatch(p_,c_,a_,n_,ref_) {
+        this.tireur.addMatch(p_,c_,a_,n_,ref_);
+        this.pointeur.addMatch(p_,c_,a_,n_,ref_);
+    }
+    setPalmares(a_,p_,c_,id_) {
+        this.tireur.setPalmares(a_,p_,c_,id_);
+        this.pointeur.setPalmares(a_,p_,c_,id_);
     }
 }
 
 class Match {
-    constructor(id_, E1_, E2_, sc1_, sc2_, type_, annee_) {
+    constructor(id_, E1_, E2_, sc1_, sc2_, type_, annee_,poule_="elimination") {
         this.id = id_;
-        this.Equipe1 = E1_;
-        this.Equipe2 = E2_;
-        this.Sc1 = sc1_;
-        this.Sc2 = sc2_;
+        this.equipes = [];
+        this.equipes.push({eq:E1_, sc:sc1_});
+        this.equipes.push({eq:E2_, sc:sc2_});
         this.type = type_;
         this.annee = annee_;
+        this.poule = poule_;
+        this.equipes[0].eq.addMatch(sc1_,sc2_,annee_,0,this);
+        this.equipes[1].eq.addMatch(sc2_,sc1_,annee_,1,this);
         this.updateELO();
     }
 
     updateELO() {
-        let ELO1 = this.Equipe1.ELOm;
-        let ELO2 = this.Equipe2.ELOm;
-        let gain = Math.abs(ELO1 - ELO2) + K;
-        let ecart = Math.abs(this.Sc1 - this.Sc2)
-        if (this.type == "Finale") gain *= 2;
-        if (this.Sc1 > this.Sc2) {
-            if (ecart>6) { 
-                this.Equipe1.update(gain*P);
-                this.Equipe2.update(-gain*P);
-            } else {
-                this.Equipe1.update(gain);
-                this.Equipe2.update(-gain);
-            }
+        let ELO1 = this.equipes[0].eq.ELOm;
+        let ELO2 = this.equipes[1].eq.ELOm;
+        let Kf = 1.;
+        let P = 1.;
+        let v=0;
+        let D = (ELO1 - ELO2); let gain=0;
+        D = Math.min(5, Math.max(-5,D));
+        let ecart = Math.abs(this.equipes[0].sc - this.equipes[1].sc)
+        let p_ = this.type.indexOf("Finale");
+        if (p_ != -1) {
+            Kf = 1.1;
+            this.equipes[0].eq.setPalmares(this.annee,0,1,this.id);
+            this.equipes[1].eq.setPalmares(this.annee,1,0,this.id);
         }
-        if (this.Sc1 < this.Sc2) {
-            if (ecart>6) { 
-                this.Equipe1.update(-gain*P);
-                this.Equipe2.update(gain*P);
-            } else {
-                this.Equipe1.update(-gain);
-                this.Equipe2.update(gain);
-            }
+        if (this.type == "Finale") Kf = 1.3;
+        if (ecart>6) P = Ke;
+
+        if (this.equipes[0].sc > this.equipes[1].sc) {
+            if (this.type == "Finale") this.equipes[0].eq.setVictoire(this.annee);
+            gain = 1 - D/Kr;
+            v=1;
         }
-    }
+        if (this.equipes[0].sc < this.equipes[1].sc) {
+            if (this.type == "Finale") this.equipes[1].eq.setVictoire(this.annee);
+            gain = -(1 +  D/ Kr);
+            v=-1;
+        }
+        if (this.equipes[0].sc == this.equipes[1].sc) {
+            gain = - D/ Kr;
+            v=0;
+        }
+        this.equipes[0].eq.update(gain*P*Kf,v);
+        this.equipes[1].eq.update(-gain*P*Kf,-v);
+}
 }
--- public/dev/Petanque_PST/js/sketch.js
+++ public/dev/Petanque_PST/js/sketch.js
@@ -4,18 +4,104 @@ let matchs = [];
 let j_json, e_json, m_json;
 let index = 0;
 let nbMatchs;
+let xM=0,yM=0;
+let id = -1;
+let mode = 1;
+let annee = 2023;
 
 function mousePressed() {
+    if (mouseX>(width-205) && mouseX<width) {
+        id = round((mouseY - 80) / 20);
+        if ( id<0 || id>=joueurs.length) id=-1;
+        // console.log(joueurs[id]);
+    }
 }
 
+function keyPressed() {
+    if (key=='a') {
+        annee = 2020;
+    }
+    if (key=='z') {
+        annee = 2021;
+    }
+    if (key=='e') {
+        annee = 2022;
+    }
+    if (key=='r') {
+        annee = 2023;
+    }
+    if (key=='w') {
+        mode = (mode+1)%2;
+    }
+}
+
+function mouseMoved() {
+    xM = min(mouseX,width-195);
+    yM = min(mouseY,height-50);
+}
 function preload() {
     j_json = loadJSON("./data/joueurs.json");
     e_json = loadJSON("./data/equipes.json");
     m_json = loadJSON("./data/matchs.json");
 }
 
+function drawConcours(x_, y_ ,a_) {
+    let x = x_;
+    let y = y_;
+    let m = matchs.filter( r => { return ( r.annee == a_);});
+    fill(255);
+    textAlign(LEFT,CENTER);
+    textSize(12);
+    text('GASSIN',x,y); y += 25;
+    let p = m.filter( r => { return ( r.type == "Poule" && r.poule=="Gassin"); });
+    for (let i in p ) {
+        let e1 = p[i].equipes[0].eq;
+        let e2 = p[i].equipes[1].eq;
+        text(e1.nom+' '+p[i].equipes[0].sc+' - '+p[i].equipes[1].sc+' '+e2.nom,x,y+i*22);
+    }
+    y += 15 + 6*22;
+    text('RAMATUELLE',x,y); y += 25;
+    p = m.filter( r => { return ( r.type == "Poule" && r.poule=="Ramatuelle"); });
+    for (let i in p ) {
+        let e1 = p[i].equipes[0].eq;
+        let e2 = p[i].equipes[1].eq;
+        text(e1.nom+' '+p[i].equipes[0].sc+' - '+p[i].equipes[1].sc+' '+e2.nom,x,y+i*22);
+    }
+    x += 100;
+    y = y_;
+    text('Demi Finale',x,y); y += 30;
+    p = m.filter( r => { return ( r.type == "Demi"); });
+    for (let i in p ) {
+        let e1 = p[i].equipes[0].eq;
+        let e2 = p[i].equipes[1].eq;
+        text(e1.nom+' '+p[i].equipes[0].sc+' - '+p[i].equipes[1].sc+' '+e2.nom,x,y+i*25);
+    }
+    y = y_ + 145;
+    p = m.filter( r => { return ( r.type != "Demi"  && r.type != "Poule" ); });
+    let mid = 180, dt = 25;
+    for (let i=0; i<p.length ; i++ ) {
+        let j = p.length-i-1;
+        let e1 = p[i].equipes[0].eq;
+        let e2 = p[i].equipes[1].eq;
+        fill(120,180,120);
+        rect(x+mid-dt+1,y+(3*j+1)*17-10,dt*2-2,20)
+        fill(200);
+        textAlign(CENTER,CENTER);
+        text(p[i].type,x+mid,y+(3*j)*17);
+        let sc = ' '+p[i].equipes[0].sc+' - '+p[i].equipes[1].sc+' ';
+        let e1_t = e1.tireur.nom+'/'+e1.pointeur.nom+' '+e1.nom;
+        let e2_t = e2.nom+' '+e2.tireur.nom+'/'+e2.pointeur.nom;
+        fill(255);
+        textAlign(RIGHT,CENTER);
+        text(e1_t,x+mid-dt,y+(3*j+1)*17);
+        textAlign(CENTER,CENTER);
+        text(sc,x+mid,y+(3*j+1)*17);
+        textAlign(LEFT,CENTER);
+        text(e2_t,x+mid+dt,y+(3*j+1)*17);
+    }
+}
 function setup() {
-    canvas = createCanvas(700,600);
+    canvas = createCanvas(innerWidth*0.99,700);
     canvas.parent("#canvas");
 
     for (let i in j_json) {
@@ -24,34 +110,58 @@ function setup() {
     }
     for (let i in e_json) {
         let e = e_json[i];
-        equipes.push( new Equipe(e.nom,joueurs[e.J1],joueurs[e.J2]));
+        equipes.push( new Equipe(e.nom,joueurs[e.J1],joueurs[e.J2],e.annee));
     }
     nbMatchs = Object.keys(m_json).length ;
-    // for (let i in m_json) {
-    //     let m = m_json[i];
-    //     matchs.push( new Match(i,equipes[m.E1],equipes[m.E2],m.Sc1,m.Sc2));
-    // }
-    frameRate(5);
+    let a_ = false;
+    for (let i in m_json) {
+        let m = m_json[i];
+        // console.log(m.annee);
+        matchs.push( new Match(i,equipes[m.E1],equipes[m.E2],m.Sc1,m.Sc2,m.type, m.annee, m.poule));
+        joueurs.sort( (a,b) => { return (b.ELO - a.ELO) ;});
+        if (m.type == "Finale") a_=true;
+        for (let j in joueurs) {
+            joueurs[j].setClst(j,a_,m.annee);
+        }
+        a_ = false;
+    }
+    // frameRate(1);
+    index=0;
 }
 
 
 function draw() {
-    background(200);
-    let m = m_json[index];
-    matchs.push( new Match(index,equipes[m.E1],equipes[m.E2],m.Sc1,m.Sc2,m.type, m.annee));
-    let classement = joueurs.sort( (a,b) => { if (a.ELO > b.ELO) return -1});
-    fill(255);
-    textAlign(LEFT,CENTER);
-    textSize(12);
-    for (let i in classement) {
-        text(classement[i].nom+" "+nf(classement[i].ELO,0,0), width-120, 20*i+80);
-    }
-    fill(0);
+    background(220);
+    fill(10,50,10);
     textAlign(CENTER,CENTER);
     textSize(36);
-    text(matchs[index].annee, width-80, 25);
+    text(matchs[index].annee, width-100, 25);
     textSize(14);
-    text(matchs[index].type, width-80, 60);
-    index += 1;
-    if (index>=nbMatchs) noLoop();
+    text(matchs[index].type, width-100, 55);
+
+    let w = (width-300)*1.;
+    let h = (height-40)*0.5;
+    fill(40,80,40);
+    noStroke();
+    rect(25,20,w+20,h+20);
+    for (let i in joueurs) {
+        let idx = joueurs[i].hist[index].c;
+        let elo = joueurs[i].hist[index].elo;
+        joueurs[i].show(idx, width-205, 20*(idx-1)+80);
+        if (mode==0) {
+            joueurs[i].draw(idx,joueurs.length,w,h,elo);
+        }
+    }
+    if (mode==1) drawConcours(40,40,annee);
+    if (frameCount % 5 == 0) {
+        if (index<nbMatchs-1) {
+            index += 1;
+        }
+    }
+    if (id != -1) {
+        joueurs[id].fiche(25,height/2,w,matchs);
+    }
+    stroke(130);
+    line(0,yM,width-200,yM);
+    line(xM,0,xM,height-50);
 }
