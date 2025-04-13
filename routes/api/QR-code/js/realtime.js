674f838faa3e1624489bfaed7b102d261a1fea32
e-Coucou <eric@e-coucou.com>
2025-04-14 00:08:26 +0200

    Update realtime.js

--- routes/api/QR-code/js/realtime.js
+++ routes/api/QR-code/js/realtime.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../data/rky-metrics-cred.json");
+const serviceAccount = require("../data/rky-metrics-credential.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
