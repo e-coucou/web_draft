641f3c11efb785df6947477e8e909ba5e30f9a61
e-Coucou <eric@e-coucou.com>
2025-04-26 11:22:05 +0200

    update

---
+++ public/js/service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'ecoucou-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  './data/pages.json',
+  '/favicon.ico',
+  '/vcard.html',
+  '/wallet.html',
+  './images'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
